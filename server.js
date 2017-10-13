be11c9865e4e4e5857ec01cbc4d025a619905a79
houkah26 <houkah26@gmail.com>
2017-10-13 11:11:48 -0700

    add mongodb uri from mlab

--- server.js
+++ server.js
@@ -1,4 +1,8 @@
-'use strict';
+"use strict";
+
+// MongoDB URI
+var mongodbURI =
+  "mongodb://heroku_10q558nn:qo51aifon20sqdldsadl37008b@ds011449.mlab.com:11449/heroku_10q558nn";
 
 var express = require("express");
 var app = express();
@@ -12,65 +16,53 @@ app.use(jsonParser());
 
 var mongoose = require("mongoose");
 
-mongoose.connect("mongodb://localhost:27017/qgen");
+mongoose.connect(mongodbURI);
 
 var db = mongoose.connection;
 
-db.on("error", function(err){
-	console.error("connection error:", err);
+db.on("error", function(err) {
+  console.error("connection error:", err);
 });
 
-db.once("open", function(){
-	console.log("db connection successful");
+db.once("open", function() {
+  console.log("db connection successful");
 });
 
-app.use(function(req, res, next){
-	res.header("Access-Control-Allow-Origin", "*");
-	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-	if(req.method === "OPTIONS") {
-		res.header("Access-Control-Allow-Methods", "PUT,POST,DELETE");
-		return res.status(200).json({});
-	}
-	next();
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  if (req.method === "OPTIONS") {
+    res.header("Access-Control-Allow-Methods", "PUT,POST,DELETE");
+    return res.status(200).json({});
+  }
+  next();
 });
 
 app.use("/users", routes);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next){
-	var err = new Error("Not Found");
-	err.status = 404;
-	next(err);
+app.use(function(req, res, next) {
+  var err = new Error("Not Found");
+  err.status = 404;
+  next(err);
 });
 
 // Error Handler
 
-app.use(function(err, req, res, next){
-	res.status(err.status || 500);
-	res.json({
-		error: {
-			message: err.message
-		}
-	});
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+  res.json({
+    error: {
+      message: err.message
+    }
+  });
 });
 
 var port = process.env.PORT || 3000;
 
-app.listen(port, function(){
-	console.log("Express server is listening on port", port);
+app.listen(port, function() {
+  console.log("Express server is listening on port", port);
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
